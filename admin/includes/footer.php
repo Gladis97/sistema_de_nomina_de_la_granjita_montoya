a7f4adf847214774933788cf08a878b11bfe8ae6
Gladis97 <84299749+Gladis97@users.noreply.github.com>
2024-06-01 21:01:36 -0600

    cambios en la interfaz y logos

---
+++ admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      
+    </div>
+    <strong>Copyright &copy; 2024 Control de Asistencia y Sistema de Nómina / RHU115 - CICLO 1</strong>
+</footer>
